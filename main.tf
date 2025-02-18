ed20f285d37243cec98109c85a67bcaf83fce33f
konnikamii <filipcho100@gmail.com>
2025-02-18 18:14:03 +0100

    .

--- main.tf
+++ main.tf
@@ -180,3 +180,50 @@ resource "aws_instance" "worker-node" {
   }
 }
  
+resource "aws_security_group" "security-group-lb" {
+  name        = "${var.project_name}-security-group-lb"
+  description = "Allow all traffic"
+  vpc_id      = aws_vpc.vpc.id
+ 
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+} 
+resource "aws_lb_target_group" "target-group" {
+  name     = "${var.project_name}-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id 
+}
+
+resource "aws_lb" "loadbalancer" {
+  name               = "${var.project_name}-loadbalancer"
+  internal           = false
+  load_balancer_type = "application"
+  ip_address_type    = "ipv4"
+  
+  security_groups    = [aws_security_group.security-group-lb.id] 
+  subnets            = [for subnet in aws_subnet.subnet-public : subnet.id]
+  enable_deletion_protection = false 
+}
+
+resource "aws_lb_listener" "loadbalancer-listener" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port              = "80"
+  protocol          = "HTTP" 
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target-group.arn
+  }
+} 
--- scripts/setup_k8s_manifests.sh
+++ scripts/setup_k8s_manifests.sh
@@ -10,3 +10,11 @@ kubectl create namespace my-app
 kubectl config set-context --current --namespace=my-app
 
 kubectl apply -f /home/konnik/app/k8s-manifests/
+
+# Install Ingress Nginx Controller
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx --create-namespace
+
+kubectl create deployment demo --image=httpd --port=80
+kubectl expose deployment demo
